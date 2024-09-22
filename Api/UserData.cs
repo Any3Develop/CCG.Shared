0852bb1b5eb2bfad2782ce7df72b6886d4391069
Artem Brilev <Any3Develop@Gmail.com>
2024-09-22 15:57:58 +0300

    added api userData

---
+++ Api/UserData.cs
@@ -0,0 +1,9 @@
+﻿namespace CCG.Shared.Api
+{
+    public class UserData
+    {
+        public string Id { get; set; }
+        public string AccessToken { get; set; }
+        public string UserName { get; set; }
+    }
+}
